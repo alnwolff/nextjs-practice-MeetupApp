cd4f59f8ec4f449c743c67ba7c0d6aff05f13cfb
Alan Sanchez <alnwolff@gmail.com>
2021-10-21 21:20:12 +0200

    prepare the pages to receive data from API

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "First Meetup",
+        address: "Some street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
